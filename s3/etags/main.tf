5ae6ff5cc6fff7780a54a47ccc07cc569ece6e53
Marco Ho <mho122@my.bcit.ca>
2024-08-26 18:45:28 -0700

    Create etag example with terraform

---
+++ s3/etags/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+}
+
+provider "aws" {
+    # Configuration options
+}
+
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  # generate an etag to track changes to the object
+  etag = filemd5("myfile.txt")
+}
